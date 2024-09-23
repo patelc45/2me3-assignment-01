1a4c3c6d2aeabcf4361b0299e75bf6d6db53e562
Irene Y. Yuan <ireneyeyuan@gmail.com>
2024-09-22 21:31:23 -0400

    Update test file

---
+++ test/TestElements.java
@@ -0,0 +1,41 @@
+public class TestElements {
+    public static void main(String[] args) {
+        testStaticElements();
+        testCollisions();
+    }
+
+    static void testStaticElements(){
+        
+        // Uncomment the following code when you want to test your classes
+        // Point p1 = new Point(1,1);
+		// Point p2 = new Point(4,3);
+		// LineSeg l1 = new LineSeg(new Point(2,2), new Point(5,3));
+		// LineSeg l2 = new LineSeg(p1, p2);
+		// Circle c1 = new Circle(3, 3);
+		// Circle c2 = new Circle(5, 5);
+		// Rectangle r1 = new Rectangle(3,5,3,0);
+		// Rectangle r2 = new Rectangle(1,7,6,3);
+		
+		// if(Point.getNumOfInstances()!= 4){
+		// 	System.out.println("Point.NumberOfInsances error");
+
+		// }
+		// if(LineSeg.getNumOfInstances()!= 2){
+		// 	System.out.println("LineSeg.NumberOfInsances error");
+		// }
+		// if(Circle.getNumOfInstances()!= 2){
+		// 	System.out.println("Circle.NumberOfInsances error");
+		// }
+		// if(Rectangle.getNumOfInstances()!= 2){
+		// 	System.out.println("Rectangle.NumberOfInsances error");
+		// }
+		// if(AbstractElement.getNumOfInstances()!=10){
+		// 	System.out.println("AbstractElement.NumberOfInsances error");
+		// }
+    }
+
+    // Write your own test cases for testing intersections
+    static void testCollisions(){
+
+    }
+}
